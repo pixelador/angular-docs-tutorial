bed3a88ee2b47c4de02646be204f7f1c0d98963f
Erik Lopez <erik@eriklopez.work>
2018-02-01 22:27:01 -0600

    Supplies mock Heroes data

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Cyclops' },
+  { id: 13, name: 'Night Crawler' },
+  { id: 14, name: 'Rogue' },
+  { id: 15, name: 'Jean Gray' },
+  { id: 16, name: 'Colossus' },
+  { id: 17, name: 'The Beast' },
+  { id: 18, name: 'Ice Man' },
+  { id: 19, name: 'Jubilee' },
+  { id: 20, name: 'Professor X' }
+];
